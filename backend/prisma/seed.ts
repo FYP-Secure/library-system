9c9cfa5eaf0227aba2c60f42621a5c976350a4d0
yinchuangsum <yinchuang96@gmail.com>
2022-06-16 22:39:02 +0800

    remove space

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -3,11 +3,11 @@ import {encrypt} from "../services/password.service";
 const prisma = new PrismaClient()
 
 async function main() {
-    const password = "super admin";
+    const password = "superadmin";
     const superAdmin = await prisma.user.create({
         data: {
             email: "super_admin@admin.com",
-            name: "superadmin",
+            name: "super admin",
             role: Role.SUPER_ADMIN,
             password: await encrypt(password)
         }
