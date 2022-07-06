8a3498873428c89783479741635fb544af18cfcb
yinchuangsum <yinchuang96@gmail.com>
2022-07-06 22:18:41 +0800

    increase json limit

--- backend/app.ts
+++ backend/app.ts
@@ -10,7 +10,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 app.listen(conf.PORT, () => {
     console.log(`Application Listening to Port ${conf.PORT}`)
